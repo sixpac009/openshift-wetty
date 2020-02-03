3cc8fd03e7c15c45c0ac82f0096f1675f4b6dc04
Jeremy Keen <jeremy@alienonesecurity.com>
2020-02-03 11:53:50 -0500

    initial commit

---
+++ fedora-profile.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export GOPATH=/home/default/go
+export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk
+export PATH=$PATH:$JAVA_HOME:$GOPATH/bin
---
+++ rhel-profile.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+export M2_HOME=/usr/bin/apache-maven-3.5.2
+export M2=$M2_HOME/bin
+export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk
+export GOPATH=/homde/default/go
+export PATH=$PATH:$JAVA_HOME:$M2:$GOPATH/bin
